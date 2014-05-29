eb34a6cfdfdc56274b185569147ba231b83656d5
Gaston <Gaston@LenoCat>
2014-05-28 21:36:21 -0300

    Me tome un gramo de merca

---
+++ Merca1/src/merca1/Merca1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package merca1;
+
+/**
+ *
+ * @author Gaston
+ */
+public class Merca1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}

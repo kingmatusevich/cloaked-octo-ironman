3ce6c2d42247448d23ab61abaa492ccc54fd1a01
Gaston <Gaston@LenoCat>
2014-05-29 01:01:18 -0300

    FUNCA WACHEM

--- Merca1/src/merca1/EventsIde.java
+++ Merca1/src/merca1/EventsIde.java
@@ -4,40 +4,76 @@
  */
 package merca1;
 
-import java.awt.Event;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  *
  * @author Gaston
  */
-public class EventsIde implements ActionListener, ItemListener, Runnable{
+public class EventsIde implements ActionListener, ItemListener, KeyListener{
     Ide iface;
+    String currentOperation=null;
+    boolean chancha;
+    CalculadoraController c = new CalculadoraController();
     public EventsIde(Ide iface){
         this.iface=iface;
     }
     
+    private double executeOperation(String currentOperation){
+        switch(currentOperation){
+            case "+": return c.add();
+            case "-": return c.substract();
+            case "/": return c.division();
+            case "*": return c.multiply();
+        }
+        return 0;
+    }
+    
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         String comando = e.getActionCommand();
-        Calculadora c = new Calculadora();
-        if(comando.equals("+")){
-            iface.txtNumbers.setText("quere suma vo?");
+        c.addOperand(Double.parseDouble(iface.txtNumbers.getText()));
+        if(currentOperation!=null) {
+                iface.txtNumbers.setText(String.valueOf(executeOperation(currentOperation)));
         }
+        currentOperation=comando;
+        if(comando.equals("=")){
+            currentOperation=null;
+        }
+        iface.txtNumbers.requestFocus();
+        chancha=true;
         
     }
 
     @Override
     public void itemStateChanged(ItemEvent e) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
     }
 
     @Override
-    public void run() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void keyTyped(KeyEvent e) {
+        
     }
-    
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+       if(chancha){
+           iface.txtNumbers.setText("");
+           chancha=false;
+           if(currentOperation==null)
+                c.clear();
+       }
+           
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+    }
 }

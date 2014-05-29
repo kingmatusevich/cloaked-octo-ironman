74b20b52d9d26e70c4d78d64748dc1ab0100f51b
Gaston <Gaston@LenoCat>
2014-05-28 21:37:13 -0300

    Me tome dos gramos de merca

--- Merca1/src/merca1/Merca1.java
+++ Merca1/src/merca1/Merca1.java
@@ -14,6 +14,6 @@ public class Merca1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("merca");
     }
 }
